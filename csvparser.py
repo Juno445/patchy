50b3ee43dda9e0adf830eee78e1115dc11f1b372
Juno445 <mellophone445@gmail.com>
2025-03-14 14:24:06 -0400

    Update csvparser.py

--- csvparser.py
+++ csvparser.py
@@ -1,7 +1,7 @@
 import  pandas as pd
 
 df = pd.read_csv('input.csv')
-
+#Breaks our values down to 100 per group
 filtered = df.groupby('agent_group').head(100)
 
 result = filtered[['agent_group','subject']]
